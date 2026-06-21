9ab561c391c01998ce6e10d577dda846abc7ff45
Uttam Mistry <uttammistry54@gmail.com>
2026-06-21 15:21:18 +0530

    Portfolio website

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
